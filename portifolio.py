6562cfa75f6bf13816a866c6c4663b997ca31f1e
Diana Avila <missernightangel@gmail.com>
2022-09-20 21:14:38 -0300

    up

--- portifolio.py
+++ portifolio.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/projects/')
+def projects():
+  return render_template('projects.html')
+
 @app.route('/about/')
 def about():
   return render_template('about.html')
